d5eb8afa444c10a441db171b888033631d3e42c3
TanoueMasaki <143382929+TanoueMasaki@users.noreply.github.com>
2023-12-22 15:19:36 +0900

    Update Sample.java
    12/22

--- app/src/main/java/com/example/myapplication/Sample.java
+++ app/src/main/java/com/example/myapplication/Sample.java
@@ -3,92 +3,62 @@ package com.example.myapplication;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import com.example.myapplication.Calculation.Operator;
+import com.example.myapplication.SetCalc.Operator;
 
 public class Sample {
 
 	public static void main(String[] args) {
+		//*アプリ用に要改修
+		//アプリ画面表示用のサンプル
+		
 		//各クラスをインスタンス化する
-		Scanner stdin1 = new Scanner(System.in);
-		Calculation calc1 = new Calculation();
+		Scanner stdin = new Scanner(System.in);
+		SetCalc setCalc = new SetCalc();
+		Calculation calc = new Calculation();
 		
 		//数値の範囲を決定する
 		//初期値から最終値までをarraySetに引数として渡す
 		//引数はユーザーが決めれるようにする
-		ArrayList<Integer> array = calc1.arraySet(1,3);
+		ArrayList<Integer> array = setCalc.arraySet(1,3);
 		
 		//解答結果を入れる配列を用意する
-		ArrayList<ArrayList<String>> results = new ArrayList<>();
+		String[][] result = new String[5][array.size() * array.size()];
 		
 		System.out.println(array);//消す
 		
 		//どの演算にするか決定する
+		//実際にはボタンを押して決定する
 		int ope = 0;
 		do {
 		System.out.println("計算方法を入力してください");
 		System.out.println("たし算:1,ひき算:2,かけ算:3,わり算:4");
-		ope = stdin1.nextInt();
+		ope = stdin.nextInt();
 		}while (!(ope == 1 || ope == 2 || ope == 3 || ope == 4));
 		
 		switch(ope) {
 			case 1:
-				results = calcDecision(array,Operator.addition);
+				result = calc.calcDecision(array,Operator.addition);
 				break;
 			case 2:
-				results = calcDecision(array,Operator.subtraction);
+				result = calc.calcDecision(array,Operator.subtraction);
 				break;
 			case 3:
-				results = calcDecision(array,Operator.multiplication);
+				result = calc.calcDecision(array,Operator.multiplication);
 				break;
 			case 4:
-				results = calcDecision(array,Operator.division);
+				result = calc.calcDecision(array,Operator.division);
 				break;
 		}
-		stdin1.close();//スキャナー１終了
+		stdin.close();//スキャナー終了
 		
-		
-		System.out.println(results);
-	
-	}
-	public static ArrayList<ArrayList<String>> calcDecision(ArrayList<Integer> array,Operator operator) {
-		Scanner stdin2 = new Scanner(System.in);
-		Calculation calc2 = new Calculation();
-		
-		String[][] results = String[array.size()][5];
-		
-		
-		
-		int questionNum = 1;
-		
-		for(int i = 0;i < array.size();i++) {
-			calc2.leftValueSet(array.get(i));
-			ArrayList<String> result = new ArrayList<>();
-			for(int j = 0;j < array.size();j++) {
-				calc2.rightValueSet(array.get(j));
-				int leftVal = calc2.getLeftValue();
-				int rightVal = calc2.getRightValue();
-				calc2.setOpe(operator);
-				String ope = calc2.getOpe();
-				System.out.print(leftVal + ope + rightVal + "=");
-					result.add(questionNum+"問");
-					int answer = stdin2.nextInt();
-					if(calc2.getAnswer() == answer) {
-						System.out.println("正解");
-						result.add("正解");
-					}else {
-						System.out.println("不正解");
-						result.add("不正解");
-					}
-					result.add(leftVal+ope+rightVal);
-					result.add(String.valueOf(calc2.getAnswer()));
-					result.add(String.valueOf(answer));
-					
-					questionNum++;
-					results.add(result);
-					result.clear();
+		//解答結果を表示する
+		for(int i = 0;i < result.length;i++) {
+			for(int j = 0;j < 5;j++) {
+			System.out.print(result[i][j] + " ");
 			}
+			System.out.println();
 		}
-		stdin2.close();//スキャナー２終了
-		return results;
 	}
+	
+	
 }
