2d45f88ad855d36ee216f488aff8f04f690fa789
TANOUE <diveeeeen1010@gmail.com>
2023-12-23 00:42:00 +0900

    a

--- app/src/main/java/com/example/myapplication/CalcPage.java
+++ app/src/main/java/com/example/myapplication/CalcPage.java
@@ -8,8 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-//import com.example.study.Calculation.*;
-
+import com.example.myapplication.SetCalc.Operator;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -17,78 +16,58 @@ public class CalcPage extends AppCompatActivity {
     //インスタンス化する
     Calculation calc = new Calculation();
     SetCalc setCalc = new SetCalc();
-
     //数値の範囲を決定する
     //初期値から最終値までをarraySetに引数として渡す
     //引数はユーザーが決めれるようにする
     ArrayList<Integer> array = setCalc.arraySet(1,3);
-
     //解答結果を入れる配列を用意する
     String[][] result = new String[5][array.size() * array.size()];
-
     //演算選択に必要
     private int ope = 0;
     public void setOpe(int ope){
         this.ope = ope;
     }
-
+    //解答を取得して格納用
+    private int answer;
+    public int getAnswer(){
+        return this.answer;
+    }
+    public void setAnswer(int ans){
+        this.answer = ans;
+    }
+    //正誤判定後に○×を表示させる
+    public void setJudgment(String judgment){
+        TextView setJudgment = findViewById(R.id.judgment);
+        setJudgment.setText(judgment);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc_page);
-
         switch(ope) {
             case 1:
-                result = calc.calcDecision(array, SetCalc.Operator.addition);
+                result = calc.calcDecision(array, Operator.addition);
                 break;
             case 2:
-                result = calc.calcDecision(array, SetCalc.Operator.subtraction);
+                result = calc.calcDecision(array, Operator.subtraction);
                 break;
             case 3:
-                result = calc.calcDecision(array, SetCalc.Operator.multiplication);
+                result = calc.calcDecision(array, Operator.multiplication);
                 break;
             case 4:
-                result = calc.calcDecision(array, SetCalc.Operator.division);
+                result = calc.calcDecision(array, Operator.division);
                 break;
         }
-
         /*判定ボタン*/
         Button hantei1 = findViewById(R.id.hantei1);
         hantei1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                calc.addition();
-                int answer = calc.answer;
-
-                EditText edNum1 = findViewById(R.id.edNum1);
-                int i = edNum1.length();
-
-                //Object.nonNull()だと上手くいかない
-                if (i != 0) {
-                    String edNum1_1 = edNum1.getText().toString();
-                    //↑String edNum1_1 = String.valueOf(edNum1);だとエラーになる
-                    int edNum1_2 = Integer.parseInt(edNum1_1);
-
-                    if (edNum1_2 == answer) {
-                        System.out.println("正解");
-                        addLeftRight();
-                        edNum1.setText(null);
-                    } else {
-                        System.out.println("不正解");
-                        addLeftRight();
-                        edNum1.setText(null);
-                    }
-
-                    leftValue(l);
-                    rightValue(r);
-
-                } else {
-                    System.out.println("入力して下さい");
-                }
+                TextView ans = findViewById(R.id.answer);
+                String ansStr = ans.getText().toString();
+                setAnswer(Integer.parseInt(ansStr));
             }
         });
-
         /*トップに戻るボタン*/
         Button buttonFinish = findViewById(R.id.buttonFinish);
         buttonFinish.setOnClickListener(new View.OnClickListener() {
@@ -97,37 +76,5 @@ public class CalcPage extends AppCompatActivity {
                 finish();
             }
         });
-
-    }
-    public void leftValue(int l) {
-
-        calc.leftValueSet(l);
-        int left = calc.leftValue;
-
-        int leftValueInt = left;
-        String leftValueStr = String.valueOf(leftValueInt);
-        TextView leftValue = findViewById(R.id.leftValue);
-        leftValue.setText(leftValueStr);
-    }
-    public void rightValue(int r) {
-
-        calc.rightValueSet(r);
-        int right = calc.rightValue;
-
-        int rightValueInt = right;
-        String rightValueStr = String.valueOf(rightValueInt);
-        TextView rightValue = findViewById(R.id.rightValue);
-        rightValue.setText(rightValueStr);
-    }
-    public void addLeftRight(){
-        if (r < 9) {
-            r++;
-        } else if(l < 9) {
-            l++;
-            r = 1;
-        } else {
-            System.out.println("計算を終了します");
-            finish();
-        }
     }
 }
--- app/src/main/java/com/example/myapplication/Calculation.java
+++ app/src/main/java/com/example/myapplication/Calculation.java
@@ -1,64 +1,61 @@
 package com.example.myapplication;
 
+import static android.text.method.Touch.onTouchEvent;
+import android.view.MotionEvent;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 import com.example.myapplication.SetCalc.Operator;
 
-public class Calculation {
+public class Calculation extends AppCompatActivity {
 	//*アプリ用に要改修
 	//計算用クラス
-	
-	public String[][] calcDecision(ArrayList<Integer> array,Operator operator) {
-		//実際には不要
-		Scanner stdin = new Scanner(System.in);
-		
-		SetCalc calc = new SetCalc();
-		
-		//解答結果を入れる配列を用意する
-		String[][] result = new String[array.size() * array.size()][5];
-		
-		//問題番号の初期化
-		int questionNum = 1;
-		
-		//ユーザーが設定した範囲arrayの総当たり計算
-		//右辺が1週したら左辺を+1する
-		for(int i = 0;i < array.size();i++) {
-			//左辺に代入する
-			//xmlのid:leftValueにsetが必要
-			calc.leftValueSet(array.get(i));
-			
-			for(int j = 0;j < array.size();j++) {
-				//右辺に代入
-				calc.rightValueSet(array.get(j));
-				int leftVal = calc.getLeftValue();
-				int rightVal = calc.getRightValue();
-				calc.setOpe(operator);
-				String ope = calc.getOpe();
-				System.out.print(leftVal + ope + rightVal + "=");
-				//問題番号を配列に入れる
-				result[questionNum - 1][0] = questionNum + "問目";
-				//入力結果を解答用の変数answerに入れる
-				//実際にはxmlからfindViewでid:answerから取得する
-				int answer = stdin.nextInt();
-				//正誤判定後、結果を配列に入れる
-				if(calc.getAnswer() == answer) {
-					System.out.println("正解");
-					result[questionNum - 1][1] = "正解";
-				}else {
-					System.out.println("不正解");
-					result[questionNum - 1][1] = "不正解";
-				}
-				//問題、正解、入力した数値を配列に入れる
-				result[questionNum - 1][2] = "問題:" + leftVal + ope + rightVal;
-				result[questionNum - 1][3] = "正解:" + calc.getAnswer();
-				result[questionNum - 1][4] = "入力した数値:" + answer;
-				//問題番号に+1する
-				questionNum++;
+	SetCalc calc = new SetCalc();
+	CalcPage calcPage = new CalcPage();
+	public String[][] calcDecision (ArrayList < Integer > array, Operator operator){
+	//解答結果を入れる配列を用意する
+	String[][] result = new String[array.size() * array.size()][5];
+	//問題番号の初期化
+	int questionNum = 1;
+	//ユーザーが設定した範囲arrayの総当たり計算
+	//右辺が1週したら左辺を+1する
+	for (int i = 0; i < array.size(); i++) {
+		//左辺に代入する
+		//xmlのid:leftValueにsetが必要
+		calc.leftValueSet(array.get(i));
+		for (int j = 0; j < array.size(); j++) {
+			//右辺に代入
+			calc.rightValueSet(array.get(j));
+			int leftVal = calc.getLeftValue();
+			int rightVal = calc.getRightValue();
+			calc.setOpe(operator);
+			String ope = calc.getOpe();
+			System.out.print(leftVal + ope + rightVal + "=");
+			//問題番号を配列に入れる
+			result[questionNum - 1][0] = questionNum + "問目";
+			//id:answerから取得して変数answerに入れる
+			int answer = calcPage.getAnswer();
+			//正誤判定後、結果を配列に入れる
+			if (calc.getAnswer() == answer) {
+				calcPage.setJudgment("○");
+				result[questionNum - 1][1] = "正解";
+			} else {
+				calcPage.setJudgment("×");
+				result[questionNum - 1][1] = "不正解";
+			}
+			//問題、正解、入力した数値を配列に入れる
+			result[questionNum - 1][2] = "問題:" + leftVal + ope + rightVal;
+			result[questionNum - 1][3] = "正解:" + calc.getAnswer();
+			result[questionNum - 1][4] = "入力した数値:" + answer;
+			//問題番号に+1する
+			questionNum++;
+			try {
+				Thread.sleep(2000);
+			}catch(InterruptedException e){
 			}
 		}
-		stdin.close();//スキャナー終了
-		return result;//解答結果配列を返す
 	}
-
+	return result;//解答結果配列を返す
+	}
 }
