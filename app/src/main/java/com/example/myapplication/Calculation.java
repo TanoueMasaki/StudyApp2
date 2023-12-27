bdebe2e02e8b336b8c5b5385fc61845b1191f992
TanoueMasaki <143382929+TanoueMasaki@users.noreply.github.com>
2023-12-27 16:22:30 +0900

    Update Calculation.java
    a

--- app/src/main/java/com/example/myapplication/Calculation.java
+++ app/src/main/java/com/example/myapplication/Calculation.java
@@ -1,61 +1,59 @@
 package com.example.myapplication;
 
-import static android.text.method.Touch.onTouchEvent;
-import android.view.MotionEvent;
-import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
-import java.util.Scanner;
-import com.example.myapplication.SetCalc.Operator;
-
-public class Calculation extends AppCompatActivity {
-	//*アプリ用に要改修
-	//計算用クラス
-	SetCalc calc = new SetCalc();
-	CalcPage calcPage = new CalcPage();
-	public String[][] calcDecision (ArrayList < Integer > array, Operator operator){
-	//解答結果を入れる配列を用意する
-	String[][] result = new String[array.size() * array.size()][5];
-	//問題番号の初期化
-	int questionNum = 1;
-	//ユーザーが設定した範囲arrayの総当たり計算
-	//右辺が1週したら左辺を+1する
-	for (int i = 0; i < array.size(); i++) {
-		//左辺に代入する
-		//xmlのid:leftValueにsetが必要
-		calc.leftValueSet(array.get(i));
-		for (int j = 0; j < array.size(); j++) {
-			//右辺に代入
-			calc.rightValueSet(array.get(j));
-			int leftVal = calc.getLeftValue();
-			int rightVal = calc.getRightValue();
-			calc.setOpe(operator);
-			String ope = calc.getOpe();
-			System.out.print(leftVal + ope + rightVal + "=");
-			//問題番号を配列に入れる
-			result[questionNum - 1][0] = questionNum + "問目";
-			//id:answerから取得して変数answerに入れる
-			int answer = calcPage.getAnswer();
-			//正誤判定後、結果を配列に入れる
-			if (calc.getAnswer() == answer) {
-				calcPage.setJudgment("○");
-				result[questionNum - 1][1] = "正解";
-			} else {
-				calcPage.setJudgment("×");
-				result[questionNum - 1][1] = "不正解";
-			}
-			//問題、正解、入力した数値を配列に入れる
-			result[questionNum - 1][2] = "問題:" + leftVal + ope + rightVal;
-			result[questionNum - 1][3] = "正解:" + calc.getAnswer();
-			result[questionNum - 1][4] = "入力した数値:" + answer;
-			//問題番号に+1する
-			questionNum++;
-			try {
-				Thread.sleep(2000);
-			}catch(InterruptedException e){
-			}
-		}
-	}
-	return result;//解答結果配列を返す
+//計算用クラス
+public class Calculation {
+	
+	
+	
+	
+	
+	//列挙型で演算子を限定する
+    public enum Operator {
+        addition,
+        subtraction,
+        multiplication,
+        division
+    }
+    
+  //演算子を判定して文字列で返す
+    public String setOpe(Operator ope){
+    	String opeStr = "エラー";
+        switch (ope){
+            case addition :
+                opeStr = "+";
+            case subtraction:
+                opeStr = "-";
+            case multiplication:
+                opeStr = "×";
+            case division:
+                opeStr = "÷";
+        }
+        return opeStr;
+    }
+    
+    //計算して答えを返す
+    public int makeCalculation(Operator ope,int leftValue,int rightValue){
+    	int answer = 0;
+        switch (ope){
+            case addition :
+                answer = leftValue + rightValue;
+            case subtraction:
+                answer = leftValue - rightValue;
+            case multiplication:
+                answer = leftValue * rightValue;
+            case division:
+                answer = leftValue / rightValue;//余りを未定義
+        }
+        return answer;
+    }
+   
+    //引数（初期値から最終値）までが入った配列ArrayList型arrayを作成して返す
+    public ArrayList<Integer> arraySet(int initialVal,int finalVal) {
+    	ArrayList<Integer> array = new ArrayList<>();
+    	for(int i = initialVal;i <= finalVal;i++) {
+    		array.add(i);
+    	}
+		return array;
 	}
 }
